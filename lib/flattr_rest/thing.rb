ecaec820f2c7edba27aed0224f78cfb218938900
Joel Hansson <joel.hansson@gmail.com>
2010-10-26 13:52:36 +0200

    adding status field for thing object

--- lib/flattr_rest/thing.rb
+++ lib/flattr_rest/thing.rb
@@ -1,10 +1,10 @@
 module FlattrRest
 
   class Thing < Model
-    attr_accessor :thing_id, :created_at, :language, :url, :title, :story, :user, :category
+    attr_accessor :thing_id, :created_at, :language, :url, :title, :story, :user, :category, :status
 
     def self.from_node(node)
-      prms = auto_parse(node,[:language,:url,:title,:story])
+      prms = auto_parse(node,[:language,:url,:title,:story,:status])
       unless prms.empty?
         prms[:created_at] = safe_parse('created',node)
         prms[:thing_id] = safe_parse('id',node)
